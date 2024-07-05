a4345e81e532e2478b13ff8ce465237139d8dac3
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2024-07-04 18:33:24 -0700

    task_0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    Annotations for variables.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+        Returns sum of two variables.
+    """
+    return a + b
