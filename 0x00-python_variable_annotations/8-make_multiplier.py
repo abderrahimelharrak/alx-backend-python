b364b657b553199a3a8c736de5986b5f9d753fac
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2024-07-04 18:44:12 -0700

    task_8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+""" Complex types - functions"""
+from typing import Callable, Iterator, Union, Optional, List, Tuple
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    takes a float multiplier as argument,
+    returns a function that multiplies a float by multiplier.
+    """
+    def c(x: float) -> float:
+        """ multiplies a float by multiplier """
+        return float(x * multiplier)
+
+    return c
